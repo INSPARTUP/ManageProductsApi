ed5325dea3b09c02d65a790b31ded8f3a49c2c7e
zaki1003 <mz.merabet@esi-sba.dz>
2021-05-08 00:26:03 +0100

    make it better

---
+++ app/config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: "mongodb+srv://dakidhem:mehdimehdi@cluster0.frum1.mongodb.net/Prod?retryWrites=true&w=majority"
+};
---
+++ app/models/tutorial.model.js
@@ -0,0 +1,22 @@
+module.exports = mongoose => {
+  var schema = mongoose.Schema(
+    {
+      nom: String,
+      type: String,
+      prix: Number,
+      quantite: Number,
+      description: String,
+      imageurl: String
+    },
+   { timestamps: true }
+  );
+
+  schema.method("toJSON", function() {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const Tutorial = mongoose.model("produits", schema);
+  return Tutorial;
+};
