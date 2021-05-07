49a09e2f2af4fd44193e2b86e0830b1e944030c7
zaki1003 <mz.merabet@esi-sba.dz>
2021-05-08 00:28:12 +0100

    make it better

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ db.mongoose
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to DecoStore application." });
 });
 
 require("./app/routes/turorial.routes")(app);
